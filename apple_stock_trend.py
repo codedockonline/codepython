d850ea100a30a8932229bbf31d05b63ebc9010c3
codedockonline <kpalkikz@gmail.com>
2025-01-31 00:34:08 -0500

    Update apple_stock_trend.py

--- apple_stock_trend.py
+++ apple_stock_trend.py
@@ -9,13 +9,13 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+# Fetch stock data
 stock = yf.Ticker("AAPL")
 df = stock.history(period="5y")
 
-
+# Plot closing price trend
 plt.figure(figsize=(10,5))
 plt.plot(df['Close'], label='Closing Price')
-plt.title('Apple Stock price trend')
+plt.title("Apple Stock Price Trend")
 plt.legend()
 plt.show()
